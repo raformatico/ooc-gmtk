5dd241df50aea3f6c157cc344612c1c9d1d4863e
raformatico <raformatico@raformatico.com>
2020-07-11 23:06:31 +0200

    Primera versión de la escapada de ooc de la caja

--- player/player.gd
+++ player/player.gd
@@ -1,6 +1,9 @@
 extends RigidBody
 
 export var force : float = 10
+export var speed  : float = 10
+onready var box : StaticBody = get_parent().get_node("box") 
+onready var box_pos : Position3D = box.get_node("box_pos")
 
 var gravity : = Vector3()
 
@@ -25,17 +28,16 @@ func onetime_pressed() -> Vector3:
 	return gravity
 
 func letit_pressed() -> Vector3:
-	gravity = Vector3(0,0,0)
 	if Input.is_action_pressed("west"):
-		gravity += Vector3(0,0,-force)
+		gravity = Vector3(0,0,-force)
 	if Input.is_action_pressed("east"):
-		gravity += Vector3(0,0,force)
+		gravity = Vector3(0,0,force)
 	if Input.is_action_pressed("up"):
-		gravity += Vector3(0,force,0)
+		gravity = Vector3(0,force,0)
 	if Input.is_action_pressed("north"):
-		gravity += Vector3(force,0,0)
+		gravity = Vector3(force,0,0)
 	if Input.is_action_pressed("south"):
-		gravity += Vector3(-force,0,0)
+		gravity = Vector3(-force,0,0)
 	if not Input.is_action_pressed("west") and not Input.is_action_pressed("east") and not Input.is_action_pressed("north") and not Input.is_action_pressed("south") and not Input.is_action_pressed("up"):
 		gravity = Vector3(0,-force,0)
 	return gravity
@@ -43,11 +45,18 @@ func letit_pressed() -> Vector3:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
 	#add_central_force(letit_pressed())
+	#linear_velocity = get_direction_run()
 	add_central_force(onetime_pressed())
 	#apply_central_impulse(onetime_pressed())
 	
 	
-
+func get_direction_run() -> Vector3:
+	#Hacerlo en función de la dirección de la gravedad
+	#Devolver una velocidad solo si toca el suelo correspondiente a la gravedad
+	var pos_ooc : Vector3 = self.translation
+	var direction : Vector3 = pos_ooc - box_pos.translation
+	direction = direction * Vector3(1,0,1)
+	return direction.normalized()*speed
 
 func _on_player_body_entered(body: Node) -> void:
 	print("entra algo... " + str(body.name))
